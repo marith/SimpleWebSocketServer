5cabc9ed3fcc12696d791c908aaab62a50865ce2
marith <marit_holm1@hotmail.com>
2017-04-25 19:01:48 +0200

    handshake method

--- src/websocket/Websocket.java
+++ src/websocket/Websocket.java
@@ -4,28 +4,57 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Marit on 25.04.2017.
  */
 public class Websocket {
     ServerSocket server;
+    Socket client;
 
-    public Websocket(int port) throws IOException {
-        ServerSocket server = new ServerSocket(port);
+    public Websocket(ServerSocket server) throws IOException {
+        this.server = server;
     }
-
-
     /*CONNECT handshake
     * client handshake request
     * IF - not understood or has an incorrect value: "400 Bad Request", immediately close the socket
     * ELSE - send accept frame
     */
-    Socket client = server.accept();
+    public void handshake(){
+        try {
+            client = server.accept();
+            InputStream in = client.getInputStream();
+            String data = new Scanner(in,"UTF-8").useDelimiter("\\r\\n\\r\\n").next();
+            String searchFor ="Sec-WebSocket-Key: ";
+            Pattern word = Pattern.compile(searchFor);
+            Matcher match = word.matcher(data);
+            match.find();
+            String key = data.substring(match.end(),match.end()+24);
+
+            //IF NO MATCH --> client.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //examples
+    /*GET /chat HTTP/1.1
+    Host: example.com:8000
+    Upgrade: websocket
+    Connection: Upgrade
+    Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==
+    Sec-WebSocket-Version: 13
+    */
+
+
 
     //recieve message
-    InputStream message = client.getInputStream();
-
     //read mask
     //read lenght
     //read data
@@ -36,6 +65,18 @@ public class Websocket {
     //CLOSE handshake
     //sends closing frame --> get closing frame back --> close connection
     //recieve closing frame --> send closing frame back
+
+
+
+    //TEST
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(3001);
+        Websocket ws = new Websocket(ss);
+        ws.handshake();
+
+
+        //byte[] encoded = new byte[] {(byte)198, (byte)131, (byte)130, (byte)182, (byte)194, (byte)135};
+    }
 }
 
 
